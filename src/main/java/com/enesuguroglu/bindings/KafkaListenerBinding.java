f0677174de835d8a4847f368c2036401ac49b9b7
euguroglu <e.uguroglu@yahoo.com>
2021-11-28 15:41:10 +0300

    Initial commit

---
+++ src/main/java/com/enesuguroglu/bindings/KafkaListenerBinding.java
@@ -0,0 +1,10 @@
+package com.enesuguroglu.bindings;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+
+public interface KafkaListenerBinding {
+
+    @Input("input-channel-1")
+    KStream<String, String> inputStream();
+}
---
+++ src/test/java/com/enesuguroglu/HellostreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enesuguroglu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellostreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
